697c02806ac28659a55fca1bbd701eec3866a055
Hariprasad K H <hariprasad72172@gmail.com>
2023-02-22 09:59:53 +0530

    Trie Tree structure Learned

--- Heap.js
+++ Heap.js
@@ -91,3 +91,119 @@
 // console.log(heap.extractMin())
 
 // heap.display()
+
+
+
+class minHeap{
+    constructor(){
+        this.heap = []
+    }
+
+    insert(value){
+        this.heap.push(value)
+        this.bubbleup(this.heap.length - 1)
+    }
+
+    bubbleup(index){
+        const element = this.heap[index]
+        while(index > 0){
+            const parentIndex = Math.floor((index-1)/2)
+            const parent = this.heap[parentIndex]
+            if(element > parent){
+                break;
+            }
+            this.heap[parentIndex] = element;
+            this.heap[index] = parent
+            index = parentIndex
+        }
+    }
+
+    extractMin(){
+        const min = this.heap[0];
+        const end = this.heap.pop();
+        if(this.heap.length > 0){
+            this.heap[0] = end
+            this.bubbleDown(0)
+        }
+        return min;
+    }
+
+    bubbleDown(index){
+        const length = this.heap.length
+        const element = this.heap[index]
+        while(true){
+            const leftIndex = 2 * index + 1;
+            const rightIndex = 2 * index + 2;
+            let leftchild, rightchild;
+            let swap = null;
+            if(leftIndex < length){
+                leftchild = this.heap[leftIndex]
+                if(leftchild < element){
+                    swap = leftIndex;
+                }
+            }
+
+            if(rightIndex < length){
+                rightchild = this.heap[rightIndex]
+                if(
+                    (swap===null && rightchild <element) ||
+                    (swap!==null && rightchild < leftchild)
+                ){
+                    swap = rightIndex;
+                }
+            }
+
+            if(swap === null){
+                break;
+            }
+
+            this.heap[index] = this.heap[swap]
+            this.heap[swap] = element;
+            index = swap;
+        }
+
+        
+    }
+
+    display(){
+        console.log(this.heap)
+    }
+}
+
+
+const heap = new minHeap();
+
+heap.insert(99)
+heap.insert(54)
+heap.insert(3)
+heap.insert(7)
+heap.insert(77)
+heap.insert(35)
+heap.insert(78)
+
+console.log(heap.extractMin())
+
+heap.display()
+
+class Heap{
+    constructor(){
+        this.heap = []
+    }
+
+    insert(value){
+        this.heap.push(value)
+        this.bubbleup(this.heap.length - 1)
+    }
+
+    bubbleup(index){
+        const element = this.heap[index]
+        if(index > 0){
+            const parentIndex = Math.floor((index-1)/2)
+            const parent = this.heap[parentIndex]
+           if( element > parent){
+            
+           }
+        }
+
+    }
+}
---
+++ Trie.js
@@ -0,0 +1,62 @@
+class TrieNode{
+    constructor(){
+        this.children = {};
+        this.isWordEnd = false;
+    }
+}
+
+class Trie{
+    constructor(){
+        this.root = new TrieNode()
+    }
+
+    insert(word){
+        let node = this.root;
+        for(let i=0;i<word.length;i++){
+            const char = word[i]
+            if(!node.children[char]){
+                node.children[char] = new TrieNode()
+            }
+            node = node.children[char]
+        }
+        node.isWordEnd = true
+
+    }
+
+    search(word){
+        let node = this.root
+        for(let i=0;i<word.length;i++){
+            const char = word[i]
+            if(!node.children[char]){
+                return false;
+            }
+            node = node.children[char]
+        }
+        return node.isWordEnd;
+    }
+
+    startWith(prefix){
+        let node = this.root
+        for(let i=0;i<prefix.length;i++){
+            const char = prefix[i]
+            if(!node.children[char]){
+                return false;
+            }
+            node = node.children[char]
+        }
+        return true;
+    }
+
+    
+}
+
+const trie = new Trie()
+
+trie.insert('hari')
+trie.insert('manu')
+
+console.log(trie.search('manu'))
+console.log(trie.search('madhu'))
+
+console.log(trie.startWith('ma'))
+console.log(trie.startWith('ha'))
